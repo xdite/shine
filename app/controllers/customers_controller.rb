4ce618ff9d33016ae935cbf25b2960a0d35ea13f
xdite <xuite.joke@gmail.com>
2016-10-27 16:09:44 +0800

    Refine search

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,11 +1,15 @@
 class CustomersController < ApplicationController
   def index
-    @customers = Customer.where(
-      "lower(first_name) LIKE :first_name OR " \
-                                      "lower(last_name) LIKE :last_name OR " \
-                                      "lower(email) = :email", first_name: "pat%",
-                                                               last_name: "pat%",
-                                                               email: "pat@example.com"
-    ).order("email = 'pat@example.com' desc, last_name asc")
+    if params[:keywords].present?
+      @keywords = params[:keywords]
+      customer_search_term = CustomerSearchTerm.new(@keywords)
+      @customers = Customer.where(
+        customer_search_term.where_clause,
+        customer_search_term.where_args
+      )
+        .order(customer_search_term.order)
+    else
+      @customers = []
+    end
   end
 end
