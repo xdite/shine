a546122783440a5e40beb2903e5c04335015adad
xdite <xuite.joke@gmail.com>
2016-10-27 16:04:42 +0800

    search query

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,11 @@
 class CustomersController < ApplicationController
   def index
-    @customers = Customer.all.limit(10)
+    @customers = Customer.where(
+      "lower(first_name) LIKE :first_name OR " \
+                                      "lower(last_name) LIKE :last_name OR " \
+                                      "lower(email) = :email", first_name: "pat%",
+                                                               last_name: "pat%",
+                                                               email: "pat@example.com"
+    ).order("email = 'pat@example.com' desc, last_name asc")
   end
 end
